d0bbf9def1b642244e421a7fe8d957cad10caba0
iosorin <miroslav.osorin@gmail.com>
2020-05-01 07:27:24 +0200

    :pencil update readme

--- services/repositories/itunes.js
+++ services/repositories/itunes.js
@@ -1,26 +1,27 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const proxyurl = 'https://cors-anywhere.herokuapp.com/'
-const baseDomain = `${proxyurl}https://itunes.apple.com`
-const baseURL = `${baseDomain}/search?term=`
-const query = `${baseURL}daft&entity=album`
+const proxyurl = 'https://cors-anywhere.herokuapp.com/';
+const baseDomain = `${proxyurl}https://itunes.apple.com`;
+const baseURL = `${baseDomain}/search?term=`;
+const query = `${baseURL}daft&entity=album`;
 
 const Repository = axios.create({
-	baseURL
-})
+    baseURL
+});
 
 export default {
-	get () {
-		return Repository.get(`${query}`).then((res) => res.data.results)
-			.catch((e) => {
-				console.log(e)
-			})
-	},
-	getAlbum (word) {
-		const req = baseURL + word + '&entity=album'
-		return Repository.get(`${req}`).then((res) => res.data.results)
-			.catch((e) => {
-				console.log(e)
-			})
-	}
-}
+    get() {
+        return Repository.get(`${query}`).then((res) => res.data.results)
+            .catch((e) => {
+                console.log(e);
+            });
+    },
+    getAlbum(word) {
+        const req = baseURL + word + '&entity=album';
+
+        return Repository.get(`${req}`).then((res) => res.data.results)
+            .catch((e) => {
+                console.log(e);
+            });
+    }
+};
